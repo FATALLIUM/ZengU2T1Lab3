4c01e0ec0956234e7dc34a3fb274453d3c548fab
Rshefa <raisas@nycstudents.net>
2022-09-30 12:29:28 -0400

    Initial commit

---
+++ src/RectangleRunner.java
@@ -0,0 +1,40 @@
+public class RectangleRunner {
+    public static void main(String[] args) {
+//        Rectangle plot1 = new Rectangle(150,200);
+//        Rectangle plot2 = new Rectangle(125);
+//        Rectangle plot3 = new Rectangle();
+//
+//        int plot1L = plot1.getLength();
+//        int plot1W = plot1.getWidth();
+//        int plot1A = plot1.calculateArea();
+//
+//        int plot2L = plot2.getLength();
+//        int plot2W = plot2.getWidth();
+//        int plot2A = plot2.calculateArea();
+//
+//        int plot3L = plot3.getLength();
+//        int plot3W = plot3.getWidth();
+//        int plot3A = plot3.calculateArea();
+//
+//        plot1.setWidth(75);
+//        plot2.setWidth(75);
+//        plot3.setWidth(75);
+//
+//         plot1L = plot1.getLength();
+//         plot1W = plot1.getWidth();
+//         plot1A = plot1.calculateArea();
+//
+//         plot2L = plot2.getLength();
+//         plot2W = plot2.getWidth();
+//         plot2A = plot2.calculateArea();
+//
+//         plot3L = plot3.getLength();
+//         plot3W = plot3.getWidth();
+//         plot3A = plot3.calculateArea();
+//
+//         int totalArea = plot1A + plot2A + plot3A;
+//
+//         System.out.println("These three plots require " + totalArea + " square feet of seed.");
+
+    }
+}
